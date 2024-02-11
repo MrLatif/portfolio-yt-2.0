b21e1280aef633e730bbcdb147d0ac5999152299
MrLatif <lertekin21@epoka.edu.al>
2024-02-11 13:42:24 +0100

    Done with the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      {/* Header */}
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Latifsamil Ertekin",
+      "Guy-who-loves-Coffee.tsx",
+      "<ButLovesToCodeMore/>",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div>
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
+    </div>
+  );
+}
